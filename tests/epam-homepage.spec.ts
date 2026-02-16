6c346378c8213a664c4029278c7bcd5afb374a4d
sshchornyi <165578556+Sshchornyi@users.noreply.github.com>
2026-02-16 17:11:12 +0200

    Create tests/epam-homepage.spec.ts

---
+++ tests/epam-homepage.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM homepage - basic flows', () => {
+  test('placeholder', async ({ page }) => {
+    // TODO: full test will be added in next commit
+  });
+});
